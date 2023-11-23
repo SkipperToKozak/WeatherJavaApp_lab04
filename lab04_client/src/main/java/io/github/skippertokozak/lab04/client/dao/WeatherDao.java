5290ff79a4bb8de8a24909363bdcf49799465d25
SkipperToKozak <69249112+SkipperToKozak@users.noreply.github.com>
2023-11-23 01:41:52 +0100

    Created working client

--- lab04_client/src/main/java/io/github/skippertokozak/lab04/client/dao/WeatherDao.java
+++ lab04_client/src/main/java/io/github/skippertokozak/lab04/client/dao/WeatherDao.java
@@ -1,48 +1,140 @@
 package io.github.skippertokozak.lab04.client.dao;
 
-import io.github.skippertokozak.lab04.client.data.Stacja;
+import com.google.gson.Gson;
+import io.github.skippertokozak.lab04.client.data.Measurement;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-public class WeatherDao implements Dao<Stacja> {
-    private String pathS = "./database/skis.txt";
+public class WeatherDao implements Dao<Measurement> {
+    private String pathS = "./database/weather_data.txt";
+    String apiURL = "https://danepubliczne.imgw.pl/api/data/synop";
+    private Measurement[] downloadedData;
+    private List<Measurement> localData = new ArrayList<>();
+
+
 
     @Override
-    public List<Stacja> getAll() {
-        List<Stacja> tempArray = new ArrayList<>();
+    public List<Measurement> getAll() {
 
-        Path sciezkaDoPliku = Path.of(pathS);
-        FileChannel channel = null;
+
+//        Path sciezkaDoPliku = Path.of(pathS);
+//        FileChannel channel = null;
+//        try {
+//            channel = FileChannel.open(sciezkaDoPliku);
+//            List<String> linie = Files.readAllLines(sciezkaDoPliku);
+//            linie.remove("\n");
+//            Gson g = new Gson();
+//
+//            for (String linia : linie) {
+//
+//                Measurement[] measurements = g.fromJson(linia, Measurement[].class);
+//                List<Measurement> tempArray = new ArrayList<>(Arrays.asList(measurements));
+//                local_data.addAll(tempArray);
+////                System.out.println(linia);
+////                String[] s = linia.split(" ");
+////                Integer id_stacji = Integer.parseInt(s[0]);
+////                String nazwa_stacji = s[1];
+////                String data_pomiaru = s[2];
+////                Integer godzina_pomiaru = Integer.parseInt(s[3]);
+////                Float temperatura = Float.parseFloat(s[4]);
+////                Integer predkosc_wiatru = Integer.parseInt(s[5]);
+////                Integer kierunek_wiatru = Integer.parseInt(s[6]);
+////                Float wilgotnosc_wzgledna = Float.parseFloat(s[7]);
+////                Float suma_opadu = Float.parseFloat(s[8]);
+////                Float cisnienie = Float.parseFloat(s[9]);
+//
+////                tempArray.add(new Measurement(id_stacji, nazwa_stacji, data_pomiaru,
+////                        godzina_pomiaru, temperatura, predkosc_wiatru,
+////                        kierunek_wiatru, wilgotnosc_wzgledna, suma_opadu, cisnienie));
+//                //System.out.println(linia);
+//            }
+//            for (Measurement m : local_data){
+//                System.out.println(m.toString());
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                if (channel != null) {
+//                    channel.close(); // Ręczne zamknięcie kanału pliku
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        return localData;
+
+    }
+
+    @Override
+    public void update(Measurement pomiar) {
+
+    }
+
+    @Override
+    public void delete(Measurement pomiar) {
+
+    }
+
+    @Override
+    public Optional<Measurement> get(Integer id) {
+        return Optional.ofNullable((localData.get(id)));
+    }
+    public void expand(String string){
+
+        Path path = Path.of(pathS);
+
         try {
-            channel = FileChannel.open(sciezkaDoPliku);
-            List<String> linie = Files.readAllLines(sciezkaDoPliku);
-            linie.remove("\n");
+            // Sprawdzamy, czy plik istnieje, jeśli nie, to go tworzymy
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
 
-            for (String linia : linie) {
-                System.out.println(linia);
-                String[] s = linia.split(" ");
-                Integer id_stacji = Integer.parseInt(s[0]);
-                String nazwa_stacji = s[1];
-                LocalDate data_pomiaru = LocalDate.parse(s[2]);
-                Integer godzina_pomiaru = Integer.parseInt(s[3]);
-                Float temperatura = Float.parseFloat(s[4]);
-                Integer predkosc_wiatru = Integer.parseInt(s[5]);
-                Integer kierunek_wiatru = Integer.parseInt(s[6]);
-                Float wilgotnosc_wzgledna = Float.parseFloat(s[7]);
-                Float suma_opadu = Float.parseFloat(s[8]);
-                Float cisnienie = Float.parseFloat(s[9]);
+            // Tworzymy obiekt z tekstem do zapisania w pliku
+            byte[] lineBytes = (string + System.lineSeparator()).getBytes();
 
-                tempArray.add(new Stacja(id_stacji, nazwa_stacji, data_pomiaru,
-                        godzina_pomiaru, temperatura, predkosc_wiatru,
-                        kierunek_wiatru, wilgotnosc_wzgledna, suma_opadu, cisnienie));
-                //System.out.println(linia);
+            // Dopisujemy linię do pliku
+            Files.write(path, lineBytes, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void loadLocalData() {
+
+        Path path = Path.of(pathS);
+        FileChannel channel = null;
+
+        try {
+            if(Files.size(Path.of(pathS))!= 0) {
+
+
+                channel = FileChannel.open(path);
+                List<String> linie = Files.readAllLines(path);
+                linie.remove("\n");
+                Gson g = new Gson();
+
+                for (String linia : linie) {
+
+                    Measurement[] measurements = g.fromJson(linia, Measurement[].class);
+                    addToLocalDate(measurements);
+                }
             }
 
         } catch (IOException e) {
@@ -55,22 +147,74 @@ public class WeatherDao implements Dao<Stacja> {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+    }
+
+    }
+    public void checkIfUpToDate(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+            loadLocalData();
+
+        // Parsujemy stringa do obiektu LocalDate
+        if(localData.size() == 0){
+
+            System.out.println("Nie znaleziono danych lokalnych\nRozpoczynam pobieranie danych");
+            addToLocalDate(downloadData());
+
         }
-        return tempArray;
+
+        LocalDate lastUpdate = LocalDate.parse(localData.get(localData.size()-1).getData_pomiaru(), formatter);
+        if (!lastUpdate.equals(LocalDate.now())){
+
+            System.out.println("Dane nie są aktualne");
+            addToLocalDate(downloadData());
+
+        }
+        else {
+            System.out.println("Dane są aktualne");
+        }
+
+
+    }
+    public Measurement[] downloadData(){
+        Gson g = new Gson();
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(getURI(apiURL))
+                .header("Content-Type", "application/json")
+                .build();
+        try {
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                String responseBody = response.body();
+                expand(responseBody);
+                Measurement[] stations = g.fromJson(responseBody, Measurement[].class);
+                return stations;
+            } else {
+                System.out.println("Failed to fetch data. Status code: " + response.statusCode());
+                return null;
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+        private static URI getURI(String url) {
+            try {
+                return new URI(url);
+            } catch (URISyntaxException e) {
+                throw new IllegalArgumentException("Invalid URL: " + url, e);         }
+        }
+        public void addToLocalDate(Measurement[] temp){
+            localData.addAll(Arrays.asList(temp));
+        }
+
+
+//            URL url = new URL("https://danepubliczne.imgw.pl/api/data/synop");
+//            InputStreamReader reader = new InputStreamReader(url.openStream());
+//            Gson gson = new Gson();
+//            PomiarExternal[] data =  gson.fromJson(reader, PomiarExternal[].class);
+//            return data;
     }
 
-    @Override
-    public void update(Stacja stacja) {
-
-    }
-
-    @Override
-    public void delete(Stacja stacja) {
-
-    }
-
-    @Override
-    public Optional<Stacja> get(Integer id) {
-        return Optional.empty();
-    }
-}
---
+++ lab04_client/src/main/java/io/github/skippertokozak/lab04/client/data/Measurement.java
@@ -0,0 +1,47 @@
+package io.github.skippertokozak.lab04.client.data;
+
+
+public class Measurement {
+    private Integer id_stacji, godzina_pomiaru, predkosc_wiatru, kierunek_wiatru;
+    private String stacja, data_pomiaru;
+    private Float temperatura, wilgotnosc_wzgledna, suma_opadu, cisnienie;
+
+    public String getData_pomiaru() {
+        return data_pomiaru;
+    }
+
+    public Measurement(Integer id_stacji, String stacja,
+                       String data_pomiaru, Integer godzina_pomiaru,
+                       Float temperatura, Integer predkosc_wiatru,
+                       Integer kierunek_wiatru, Float wilgotnosc_wzgledna,
+                       Float suma_opadu, Float cisnienie) {
+
+        this.id_stacji = id_stacji;
+        this.stacja = stacja;
+        this.data_pomiaru = data_pomiaru;
+        this.godzina_pomiaru = godzina_pomiaru;
+        this.temperatura = temperatura;
+        this.predkosc_wiatru = predkosc_wiatru;
+        this.kierunek_wiatru = kierunek_wiatru;
+        this.wilgotnosc_wzgledna = wilgotnosc_wzgledna;
+        this.suma_opadu = suma_opadu;
+        this.cisnienie = cisnienie;
+    }
+
+    @Override
+    public String toString() {
+        return "Measurement{" +
+                "id_stacji=" + id_stacji +
+                ", godzina_pomiaru=" + godzina_pomiaru +
+                ", predkosc_wiatru=" + predkosc_wiatru +
+                ", kierunek_wiatru=" + kierunek_wiatru +
+                ", data_pomiaru=" + data_pomiaru +
+                ", nazwa_stacji='" + stacja + '\'' +
+                ", temperatura=" + temperatura +
+                ", wilgotnosc_wzgledna=" + wilgotnosc_wzgledna +
+                ", suma_opadu=" + suma_opadu +
+                ", cisnienie=" + cisnienie +
+                '}';
+    }
+
+}
